e46802aa8f8f1dc31c8a59fcbf393245773be9d3
Nickie Potter <43359989+chickienickie@users.noreply.github.com>
2026-03-15 18:44:39 -0400

    updating styling adding pages

---
+++ schedule.js
@@ -0,0 +1,27 @@
+const schedules = {
+
+    U9:[
+    
+    {date:"Apr 11",home:"PVYS5",away:"BEYS3",field:"PV3"},
+    {date:"Apr 11",home:"BEYS1",away:"BEYS2",field:"BEYS1"},
+    
+    {date:"Apr 18",home:"BEYS1",away:"PVYS5",field:"BEYS1"},
+    {date:"Apr 18",home:"BEYS2",away:"BEYS3",field:"BEYS2"}
+    
+    ],
+    
+    U11:[
+    
+    {date:"Apr 11",home:"BSA3",away:"BEYS1",field:"BSA Governors"},
+    {date:"Apr 11",home:"BEYS2",away:"BEYS3",field:"BEYS1"}
+    
+    ],
+    
+    U13:[
+    
+    {date:"Apr 11",home:"BEYS1",away:"WBS1",field:"BEYS1"},
+    {date:"Apr 11",home:"WBS2",away:"BEYS2",field:"WB1"}
+    
+    ]
+    
+    }
