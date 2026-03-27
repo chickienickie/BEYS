79f755bcd5edddcfb139553bf7d37e6f8f5d08d2
Nickie Potter <43359989+chickienickie@users.noreply.github.com>
2026-03-27 19:33:27 -0400

    updated styling for schedules and other pages

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
 function toggleMenu(){
-
-    document.getElementById("nav").classList.toggle("active")
-    
-    }
-    
-    const ageFilter=document.getElementById("ageFilter")
-    const teamFilter=document.getElementById("teamFilter")
-    
-    function loadSchedule(){
-    
-    const age=ageFilter.value
-    const team=teamFilter.value
-    
-    const table=document.getElementById("scheduleTable")
-    
-    table.innerHTML=""
-    
-    schedules[age].forEach(game=>{
-    
-    if(team==="all" || game.home===team || game.away===team){
-    
-    table.innerHTML+=`
-    <tr>
-    <td>${game.date}</td>
-    <td>${game.home}</td>
-    <td>${game.away}</td>
-    <td>${game.field}</td>
-    </tr>
-    `
-    
-    }
-    
-    })
-    
-    }
-    
-    ageFilter.addEventListener("change",loadSchedule)
-    teamFilter.addEventListener("change",loadSchedule)
-    
-    loadSchedule()
+    document.getElementById("nav").classList.toggle("active");
+  }
+  
+  const ageFilter = document.getElementById("ageFilter");
+  const teamFilter = document.getElementById("teamFilter");
+  
+  function loadSchedule(){
+  
+  const age = ageFilter.value;
+  const team = teamFilter.value;
+  const table = document.getElementById("scheduleTable");
+  
+  table.innerHTML = "";
+  
+  schedules[age].forEach(game => {
+  
+  if (team === "all" || game.home === team || game.away === team){
+  
+  table.innerHTML += `
+  <tr>
+  <td>${game.date}</td>
+  <td>${game.home}</td>
+  <td>${game.away}</td>
+  <td>${game.field}</td>
+  </tr>
+  `;
+  
+  }
+  
+  });
+  
+  }
+  
+  ageFilter.addEventListener("change", loadSchedule);
+  teamFilter.addEventListener("change", loadSchedule);
+  
+  loadSchedule();
